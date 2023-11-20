af0d72729e348c801c097d1657a0c733ba4dcd58
MAwaysa <awaysa35@gmail.com>
2023-11-20 13:16:53 +0200

    testing stream

--- resources/views/generate-qrcode.blade.php
+++ resources/views/generate-qrcode.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 @section('content')
     <div id='qrcode'>{!! $qrCode !!}</div>
-<div> {{$uniqueId}}</div>
+    <div> {{ $uniqueId }}</div>
 
-<div>chat</div>
+    <div>chat</div>
 
     <script type="module">
-
-        Echo.channel(`chat.{{$uniqueId}}`)
+        console.log(`chat.{{ $uniqueId }}`)
+        Echo.channel(`chat.{{ $uniqueId }}`)
             .listen('MessageSent', (e) => {
-            //in js remove the #qrcode and start adding to the chat ever e.message apears
+                console.log(e)
+                //in js remove the #qrcode and start adding to the chat ever e.message apears
             }).error((error) => {
                 console.error('Could not subscribe to the channel:', error);
             });
