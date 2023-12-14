836fbc1f62d2fe25cc7008d344680ff05a369d86
MAwaysa <awaysa35@gmail.com>
2023-12-14 12:13:23 +0200

    whatsaap

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -32,21 +32,49 @@ class WhatsappController extends Controller
         dd($response->json());
     }
 
+    // public function handleWebhook(Request $request)
+    // {
+    //     $mode = $request->query('hub_mode');
+    //     $token = $request->query('hub_verify_token');
+    //     $challenge = $request->query('hub_challenge');
+
+    //     Log::info('mode: ' . $mode);
+    //     Log::info('token: ' . $token);
+    //     Log::info('challenge: ' . $challenge);
+    //     Log::info('request: ' . $request);
+
+    //     return response()->json(['challenge' => $challenge], 200);
+
+    //     // Check if mode and token were sent
+
+    // }
+
+
     public function handleWebhook(Request $request)
     {
         $mode = $request->query('hub_mode');
         $token = $request->query('hub_verify_token');
         $challenge = $request->query('hub_challenge');
 
-        Log::info('mode: ' . $mode);
-        Log::info('token: ' . $token);
-        Log::info('challenge: ' . $challenge);
-        Log::info('request: ' . $request);
-        
-        return response()->json(['challenge' => $challenge], 200);
         // Check if mode and token were sent
-
+        if ($mode && $token) {
+            // Check if mode and token sent are correct
+            if ($mode == 'subscribe' && $token == 213123) {
+                // Respond with 200 OK and challenge token from the request
+                Log::info('WEBHOOK_VERIFIED');
+                return response()->json(['challenge' => $challenge], 200);
+            } else {
+                // Respond with '403 Forbidden' if verify tokens do not match
+                Log::info('VERIFICATION_FAILED');
+                return response()->json(['status' => 'error', 'message' => 'Verification failed'], 403);
+            }
+        } else {
+            // Respond with '400 Bad Request' if verify tokens do not match
+            Log::info('MISSING_PARAMETER');
+            return response()->json(['status' => 'error', 'message' => 'Missing parameters'], 400);
+        }
     }
+
 
     public function receive(Request $request)
     {
